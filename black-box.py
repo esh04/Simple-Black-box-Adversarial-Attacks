9705f77b51f3a43a8af2973e153adf47b1770319
esh04 <eshikak04@gmail.com>
2023-04-30 17:52:37 +0530

    finetuned 70 percent

--- black-box.py
+++ black-box.py
@@ -2,7 +2,7 @@ import torch
 from torch.utils.data import DataLoader
 import tqdm
 from datasets import load_dataset
-from torchvision.models import resnet50
+from torchvision.models import resnet50, ResNet50_Weights
 from torchvision import transforms
 from torchvision.datasets import ImageFolder
 import torch.nn as nn
@@ -33,19 +33,16 @@ class TinyImageNetDataset(torch.utils.data.Dataset):
         return len(self.dataset)
     
 class Resnet50TinyImageNet(nn.Module):
-  # load the pretrained ResNet50 model
-  # freeze the weights of the pre-trained layers
-  # modify the last layer to output 200 classes
     def __init__(self):
         super(Resnet50TinyImageNet, self).__init__()
-        self.model = resnet50()
+        self.model = resnet50(weights=ResNet50_Weights.DEFAULT)
         num_features = self.model.fc.in_features
         self.model.fc = nn.Linear(num_features, 200)
     
     def forward(self, x):
         return self.model(x)
     
-    def train(self, train_loader, criterion, optimizer, num_epochs=10):
+    def train(self, train_loader, val_loader, criterion, optimizer, num_epochs=10):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.model.to(device)
         
@@ -72,9 +69,26 @@ class Resnet50TinyImageNet(nn.Module):
             train_loss = train_loss / len(train_loader.dataset)
             train_acc = train_acc / len(train_loader.dataset)
 
-            print('Epoch: {} \tTraining Loss: {:.6f} \tTraining Accuracy: {:.6f}'.format(
-                epoch+1, train_loss, train_acc))
-            
+            self.model.eval()
+            val_loss = 0.0
+            val_acc = 0.0
+
+            for image, label in tqdm.tqdm(val_loader):
+                image = image.to(device)
+                label = label.to(device)
+
+                outputs = self.model(image)
+                loss = criterion(outputs, label)
+
+                val_loss += loss.item() * image.size(0)
+                _, prediction = torch.max(outputs, 1)
+                val_acc += torch.sum(prediction == label.data)
+
+            val_loss = val_loss / len(val_loader.dataset)
+            val_acc = val_acc / len(val_loader.dataset)
+
+            print("Epoch: {} \tTraining Loss: {:.6f} \tTraining Accuracy: {:.6f} \tValidation Loss: {:.6f} \tValidation Accuracy: {:.6f}".format(epoch+1, train_loss, train_acc, val_loss, val_acc))
+
 
     def test(self, test_loader):
         device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -98,15 +112,15 @@ class Resnet50TinyImageNet(nn.Module):
         print("Accuracy: ", accuracy_score(true.cpu(), predictions.cpu()))
 
         return predictions
-
             
 ## ______________
 
 # Hyperparameters
 
 BATCH_SIZE = 64
-EPOCHS = 15
+EPOCHS = 3
 LEARNING_RATE = 0.001
+MOMENTUM = 0.9
 
 ## ______________
 
@@ -133,22 +147,21 @@ val_loader = DataLoader(val_dataset, batch_size=64, shuffle=True)
 model = Resnet50TinyImageNet()
 
 criterion = nn.CrossEntropyLoss()
-optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+# optimizer = torch.optim.Adam(model.parameters(), lr=LEARNING_RATE)
+optimizer = torch.optim.SGD(model.parameters(), lr=LEARNING_RATE, momentum=MOMENTUM)
 
-model.train(train_loader, criterion, optimizer, EPOCHS)
+model.train(train_loader, val_loader, criterion, optimizer, EPOCHS)
 
-torch.save(model.state_dict(), 'res15.pth')
-pred = model.test(val_loader)
+torch.save(model.state_dict(), './model/res15.pth')
 
-with open('pred_15.pkl', 'wb') as f:
-    pickle.dump(pred, f)
+## ______________
 
-# save val laoder and train loader
-with open('val_loader_15.pkl', 'wb') as f:
-    pickle.dump(val_loader, f)
+# Test model
 
-with open('train_loader_15.pkl', 'wb') as f:
-    pickle.dump(train_loader, f)
+# pred = model.test(val_loader)
+
+# with open('pred_15.pkl', 'wb') as f:
+#     pickle.dump(pred, f)
 
 
 
